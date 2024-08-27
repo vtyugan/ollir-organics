4b8d767a929e11956f28b81cfae4e9af45b08597
vinoth kumar <vinoth@strackit.com>
2024-08-27 21:48:53 +0530

    'release'

--- src/Home/index.js
+++ src/Home/index.js
@@ -9,7 +9,7 @@ function App() {
     <div>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
+          <li><Link to="/">Home Release Testing</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
